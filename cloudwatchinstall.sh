6c974172f42bfcbec66acadfd27854059d55e80b
navaneethreddydevops <navaneethreddydevops@gmail.com>
2019-09-22 15:51:40 -0500

    adding the cloudwatch installation

---
+++ cloudwatchinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Updating the Kernel and all packages"
+sudo yum -y update 
+echo "Installing the CloudWatch Agent"
---
+++ shellifelse.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#testing the if statement
+if Iamnotacommand
+then 
+echo "It Worked"
+fi
+echo "we are outside of the if statement"
